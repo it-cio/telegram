a0744eaa3fb61003f71c3582e6b7b7bc114c1599
Divider <89965004+it-cio@users.noreply.github.com>
2021-10-04 22:35:08 +0300

    hotfix

--- bot.py
+++ bot.py
@@ -11,10 +11,15 @@ bot = Bot(token=bot_name)
 dp = Dispatcher(bot)
 
 
-async def bot_answer(message: types.Message, text):
-    await send_message(text)
+@dp.message_handler(commands=['test'])
+async def bot_answer(message: types.Message):
+    await send_message('Тест пройден!')
     await message.delete()
     await asyncio.sleep(1)
+
+
+async def fn(_):
+    print('Aiogram bot')
 
 
 async def send_message(message):
@@ -22,32 +27,32 @@ async def send_message(message):
     message_id = bot_message.message_id
     id_list.append(message_id)
     await asyncio.sleep(1)
-    print(f"Bot send message (id: {message_id})\n")
+    print(f"Bot send message (id: {message_id})")
     return message_id
 
 
 async def edit_message(message, message_id):
     await bot.edit_message_text(message, bot_id, message_id)
-    print(f"Bot edit message (id: {message_id})\n")
+    print(f"Bot edit message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 async def delete_message(message_id):
     await bot.delete_message(bot_id, message_id)
-    print(f"Bot delete message (id: {message_id})\n")
+    print(f"Bot delete message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 async def pin_message(message_id):
     await bot.pin_chat_message(bot_id, message_id)
-    print(f"Bot pin message (id: {message_id})\n")
+    print(f"Bot pin message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 # delete all pinned message
 @dp.message_handler(content_types=['pinned_message'])
 async def delete_pinned(message: types.Message):
-    print(f"Bot delete pinned_message\n")
+    print(f"Bot delete pinned_message")
     await message.delete()
 
 
@@ -62,6 +67,9 @@ async def on_shutdown(_):
     await asyncio.sleep(1)
 
 
-def run():
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
+async def close(_):
+    print('Shutdown')
 
+
+def run(startup=fn, shutdown=fn):
+    executor.start_polling(dp, skip_updates=True, on_startup=(on_startup, startup), on_shutdown=(on_shutdown, shutdown))
