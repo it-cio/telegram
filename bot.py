99aa46edfccf7212246f2f2afdfb960f1e026aa7
Divider <89965004+it-cio@users.noreply.github.com>
2021-10-04 22:18:10 +0300

    hotfix

--- bot.py
+++ bot.py
@@ -11,12 +11,10 @@ bot = Bot(token=bot_name)
 dp = Dispatcher(bot)
 
 
-async def bot_commands(name, text):
-    @dp.message_handler(commands=[name])
-    async def bot_answer(message: types.Message):
-        await send_message(text)
-        await message.delete()
-        await asyncio.sleep(1)
+async def bot_answer(message: types.Message, text):
+    await send_message(text)
+    await message.delete()
+    await asyncio.sleep(1)
 
 
 async def send_message(message):
@@ -24,25 +22,25 @@ async def send_message(message):
     message_id = bot_message.message_id
     id_list.append(message_id)
     await asyncio.sleep(1)
-    print(f"Bot send message:\n{message}\nMessage id: {message_id}\n")
+    print(f"Bot send message (id: {message_id})\n")
     return message_id
 
 
 async def edit_message(message, message_id):
     await bot.edit_message_text(message, bot_id, message_id)
-    print(f"Bot edit message:\n{message}\nMessage id: {message_id}\n")
+    print(f"Bot edit message (id: {message_id})\n")
     await asyncio.sleep(1)
 
 
 async def delete_message(message_id):
     await bot.delete_message(bot_id, message_id)
-    print(f"Bot delete message\nMessage id: {message_id}\n")
+    print(f"Bot delete message (id: {message_id})\n")
     await asyncio.sleep(1)
 
 
 async def pin_message(message_id):
     await bot.pin_chat_message(bot_id, message_id)
-    print(f"Bot pin message\nMessage id: {message_id}\n")
+    print(f"Bot pin message (id: {message_id})\n")
     await asyncio.sleep(1)
 
 
