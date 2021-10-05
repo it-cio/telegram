f2b11ceba9157f886b5299e3bd6ac678114bd769
Divider <it-cio@mail.ru>
2021-10-05 15:26:40 +0300

    fix datetime function

--- bot.py
+++ bot.py
@@ -5,17 +5,20 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
 
 
-''' DateTime '''
-date = datetime.now().strftime('%Y-%m-%d')
-time = datetime.now().strftime('%H:%M:%S')
-dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
 id_list = []
 load_dotenv()
 bot_name = os.getenv('token')
 bot_id = os.getenv('id')
 bot = Bot(token=bot_name)
 dp = Dispatcher(bot)
+
+
+def date_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+async def fn(_):
+    print('Aiogram bot')
 
 
 @dp.message_handler(commands=['test'])
@@ -25,41 +28,37 @@ async def bot_answer(message: types.Message):
     await asyncio.sleep(1)
 
 
-async def fn(_):
-    print('Aiogram bot')
-
-
 async def send_message(message):
     bot_message = await bot.send_message(bot_id, message)
     message_id = bot_message.message_id
     id_list.append(message_id)
     await asyncio.sleep(1)
-    print(f"{dt} Bot send message (id: {message_id})")
+    print(f"{date_time()} | Bot send message (id: {message_id})")
     return message_id
 
 
 async def edit_message(message, message_id):
     await bot.edit_message_text(message, bot_id, message_id)
-    print(f"{dt} Bot edit message (id: {message_id})")
+    print(f"{date_time()} | Bot edit message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 async def delete_message(message_id):
     await bot.delete_message(bot_id, message_id)
-    print(f"{dt} Bot delete message (id: {message_id})")
+    print(f"{date_time()} | Bot delete message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 async def pin_message(message_id):
     await bot.pin_chat_message(bot_id, message_id)
-    print(f"{dt} Bot pin message (id: {message_id})")
+    print(f"{date_time()} | Bot pin message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 # delete all pinned message
 @dp.message_handler(content_types=['pinned_message'])
 async def delete_pinned(message: types.Message):
-    print(f"{dt} Bot delete pinned_message")
+    print(f"{date_time()} | Bot delete pinned_message")
     await message.delete()
 
 
