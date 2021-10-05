ee4ad91636eeb9e1fc0f595e46c0b9ce3894051c
Divider <it-cio@mail.ru>
2021-10-05 12:15:43 +0300

    added datetime

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 import os
 import asyncio
+from datetime import datetime
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
+
+
+''' DateTime '''
+date = datetime.now().strftime('%Y-%m-%d')
+time = datetime.now().strftime('%H:%M:%S')
+dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 id_list = []
 load_dotenv()
@@ -27,32 +34,32 @@ async def send_message(message):
     message_id = bot_message.message_id
     id_list.append(message_id)
     await asyncio.sleep(1)
-    print(f"Bot send message (id: {message_id})")
+    print(f"{dt} Bot send message (id: {message_id})")
     return message_id
 
 
 async def edit_message(message, message_id):
     await bot.edit_message_text(message, bot_id, message_id)
-    print(f"Bot edit message (id: {message_id})")
+    print(f"{dt} Bot edit message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 async def delete_message(message_id):
     await bot.delete_message(bot_id, message_id)
-    print(f"Bot delete message (id: {message_id})")
+    print(f"{dt} Bot delete message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 async def pin_message(message_id):
     await bot.pin_chat_message(bot_id, message_id)
-    print(f"Bot pin message (id: {message_id})")
+    print(f"{dt} Bot pin message (id: {message_id})")
     await asyncio.sleep(1)
 
 
 # delete all pinned message
 @dp.message_handler(content_types=['pinned_message'])
 async def delete_pinned(message: types.Message):
-    print(f"Bot delete pinned_message")
+    print(f"{dt} Bot delete pinned_message")
     await message.delete()
 
 
@@ -65,10 +72,6 @@ async def on_shutdown(_):
     for message_id in id_list:
         await delete_message(message_id)
     await asyncio.sleep(1)
-
-
-async def close(_):
-    print('Shutdown')
 
 
 def run(startup=fn, shutdown=fn):
